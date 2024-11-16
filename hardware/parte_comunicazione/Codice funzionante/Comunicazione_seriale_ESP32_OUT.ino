77d7de56c7c2925666762c09c9497c5130291614
Al30716 <82284243+Al30716@users.noreply.github.com>
2024-11-16 20:04:21 +0100

    Add files via upload

---
+++ hardware/parte_comunicazione/Codice funzionante/Comunicazione_seriale_ESP32_OUT.ino
@@ -0,0 +1,12 @@
+#define RXp0 3
+#define TXp0 -1
+
+void setup() {
+
+  Serial.begin(115200);
+  Serial0.begin(9600, SERIAL_8N1, RXp0, TXp0);
+  
+}
+void loop() {
+    Serial.println(Serial0.readString());
+}
