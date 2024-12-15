b0be68663ae411526718563f8245e25c0c13f823
Al30716 <82284243+Al30716@users.noreply.github.com>
2024-12-15 22:51:44 +0100

    Miglioramento codice
    - aggiunto maggiore codice di debug
    - server mqtt funzionante
    -- DA IMPLEMENTARE
    - porta custom per mqtt
    - server senza internet
    - subscribe e publish di messaggi

--- hardware/parte_comunicazione/Codice funzionante/Server_mqtt_ETH.ino
+++ hardware/parte_comunicazione/Codice funzionante/Server_mqtt_ETH.ino
@@ -1,14 +1,8 @@
-
-#define ETH_CLK_MODE ETH_CLOCK_GPIO17_OUT
-#define ETH_PHY_POWER 12
-
 #include <PicoMQTT.h>
 #include <ETH.h>
 
+PicoMQTT::Server mqtt;
 static bool eth_connected = false;
-
-class MQTT: public PicoMQTT::Server {} mqtt;
-
 
 void setup() {
 
@@ -17,57 +11,56 @@ void setup() {
   Serial.begin(115200);
   Serial.print("Setup...\n");
   
-  WiFi.onEvent(WiFiEvent);
-  
-  Serial.print("Starting ETH interface...\n");
+  Serial.print("Configuring Ethernet...\n");
   ETH.begin();
-
+  NETsetup();
+  Serial.print("Network configuration done\n");
+  Serial.print("Starting MQTT server...\n");
+  //PicoMQTT::Server mqtt(1883);        porta custom
   mqtt.begin();
-  Serial.print("MQTT server started...\n");
-
+  Serial.print("MQTT server started\n");
 }
 
-void WiFiEvent(WiFiEvent_t event)
-{
-  switch (event) {
+void NETsetup() {
 
-    case ARDUINO_EVENT_ETH_CONNECTED:
-
-      Serial.println("ETH Connected");
-
-      break;
-
-    case ARDUINO_EVENT_ETH_GOT_IP:
-
-      Serial.print("Got an IP Address for ETH MAC: ");
-      Serial.print(ETH.macAddress());
-      Serial.print(", IPv4: ");
-      Serial.print(ETH.localIP());
-      if (ETH.fullDuplex()) {
-        Serial.print(", FULL_DUPLEX");
-      }
-      Serial.print(", ");
-      Serial.print(ETH.linkSpeed());
-      Serial.println("Mbps");
-      eth_connected = true;
-
-      break;
-
-    case ARDUINO_EVENT_ETH_DISCONNECTED:
-
-      Serial.println("ETH Disconnected");
-      eth_connected = false;
-
-      break;
+  while (!ETH.linkUp()) {
+    Serial.println("Waiting for Ethernet link...");
+    delay(3000);
   }
+
+  Serial.println("ETH Connected");
+  eth_connected = true;
+
+  while (ETH.localIP() == IPAddress(0, 0, 0, 0)) {
+    Serial.println("Waiting for IP address...");
+    delay(3000);
+  }
+
+  Serial.print("Got an IP Address for ETH MAC: ");
+  Serial.print(ETH.macAddress());
+  Serial.print(", IPv4: ");
+  Serial.print(ETH.localIP());
+  if (ETH.fullDuplex()) {
+    Serial.print(", FULL_DUPLEX");
+  }
+  Serial.print(", ");
+  Serial.print(ETH.linkSpeed());
+  Serial.println("Mbps");
 }
 
+int i=0;
 void loop() {
-  
-  if (random(1000) == 0){
-    mqtt.publish("picomqtt/welcome", "Hello from PicoMQTT!");
 
-    delay(1000);
-  
+  mqtt.loop();
+  if (!ETH.linkUp() || ETH.localIP() == IPAddress(0, 0, 0, 0)) {
+    Serial.println("ETH Disconnected or IP lost\nReconnecting...");
+    eth_connected = false;
+    NETsetup();
   }
+
+  if (i==1000){
+    mqtt.publish("picomqtt/welcome", "Hello from PicoMQTT!");
+    delay(1000);
+    i=0;
+  } i++;
 }
