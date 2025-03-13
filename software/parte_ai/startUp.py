b3160762ddbf29f9e377449228de1eac73fad712
ZXerniXZ <ZXerniXZ@gmail.com>
2025-03-14 00:47:14 +0100

    commitfromrasp

--- software/parte_ai/startUp.py
+++ software/parte_ai/startUp.py
@@ -1,14 +1,21 @@
 import subprocess
+import time
 
 # Lista di comandi da eseguire in parallelo
-commands = [
-    "source /home/laserlab/Desktop/Venv3/bin/activate",
-    "",
-    "df -h",
+commands_with_wait = [
+    ("source /home/laserlab/Desktop/Venv3/bin/activate", 1),
+    ("echo 'inizializzazione server mqtt...'", 0),
+    ("python3 /home/laserlab/projectDayProject/software/parte_ai/mqttStart.py", 5),
+    ("echo 'mqtt inizializzto'", 0),
+    ("echo 'inizializzazione main script...'",0),
+    ("python3 /home/laserlab/projectDayProject/software/parte_ai/raspMAIN.py",5),
+    ("echo 'MAIN inizializzato'", 0),
+    ("df -h",)
 ]
 
-processes = [subprocess.Popen(cmd, shell=True) for cmd in commands]
-
 # Attendi che tutti i processi terminino
-for p in processes:
-    p.wait()
+for cmd, delay in commands_with_wait:
+
+    subprocess.run(cmd, shell=True)
+
+    time.sleep(delay)
