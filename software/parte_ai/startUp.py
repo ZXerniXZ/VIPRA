d9d3f25ff12dc79ea72722aded01ead77f6545a1
ZXerniXZ <ZXerniXZ@gmail.com>
2025-03-14 01:28:41 +0100

    aggiornamento startup

--- software/parte_ai/startUp.py
+++ software/parte_ai/startUp.py
@@ -3,6 +3,8 @@ import time
 
 # Lista di comandi con attesa
 commands_with_wait = [
+    ("echo aggiornamento da public repo", 0)
+    ("cd projectDayProject && git pull", 10)
     ("echo 'inizializzazione server mqtt...'", 0),
     ("gnome-terminal -- bash -c 'python3 /home/laserlab/projectDayProject/software/parte_ai/mqttStart.py; exec bash'", 5),
     ("echo 'mqtt inizializzato'", 0),
