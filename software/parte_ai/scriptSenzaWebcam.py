e7d711156fa1a21b4ac6cfa2545adc9203bb5618
ZXerniXZ <127442860+ZXerniXZ@users.noreply.github.com>
2025-03-14 14:26:14 +0100

    clean up
    Delete software/parte_ai/scriptSenzaWebcam.py

--- software/parte_ai/scriptSenzaWebcam.py
+++
@@ -1,6 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO("best.pt")
-
-model.predict(source = "video.mp4", show=True, save=True, conf=0.1, line_width = 2, save_crop = True,
-              save_txt = True, show_labels = True, show_conf = True, classes=[0, 1])
